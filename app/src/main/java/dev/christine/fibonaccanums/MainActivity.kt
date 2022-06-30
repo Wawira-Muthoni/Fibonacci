69d7be7250349ab42a83f6dc193dedc6e24c1f09
Wawira-Muthoni <christinewawiramuthoni@gmail.com>
2022-06-30 09:00:53 +0300

    changes

--- app/src/main/java/dev/christine/fibonaccanums/MainActivity.kt
+++ app/src/main/java/dev/christine/fibonaccanums/MainActivity.kt
@@ -15,31 +15,23 @@ class MainActivity : AppCompatActivity() {
 
     }
    fun displayNumbers(){
-//       val n = 100
-//        var t1 = 0
-//        var t2 = 1
-//
-//        print("First $n terms: ")
-//
-//        for (i in 1..n) {
-//            print("$t1 + ")
-//
-//            val sum = t1 + t2
-//            t1 = t2
-//            t2 = sum
-//        }
+   var numberList = fibonacci(100)
+       val numberAdapter = NumbersRecyclerViewAdapter(numberList)
+       binding.rvNUmbers.layoutManager = LinearLayoutManager(this)
+       binding.rvNUmbers.adapter=numberAdapter
+   }
 
-
-    var numberList = listOf<Int>( 0, 1, 1 ,2 ,3 ,5 ,8 ,13 ,21 ,34, 55 ,89 ,144 ,233 ,377 ,610 ,987,
-        1597 ,2584, 4181 ,6765 ,10946 ,17711 ,28657 ,46368 ,75025 ,121393 ,196418, 317811 ,514229 ,
-        832040 , 1346269 ,2178309 ,3524578 ,5702887 ,9227465 ,14930352 ,24157817 ,
-        39088169 ,63245986 ,102334155 ,165580141 ,267914296 ,433494437 ,701408733 ,1134903170 )
-    val numberAdapter =NumbersRecyclerViewAdapter(numberList)
-
-    binding.rvNUmbers.layoutManager = LinearLayoutManager(this)
-     binding.rvNUmbers.adapter = numberAdapter
-
-}
+   fun fibonacci(n:Int):List<Int> {
+       var list = ArrayList<Int>()
+       var prv = 0
+       var prvone = 1
+       for (i in 0..n) {
+           var sum = prv + prvone
+           prv = prvone
+           prvone = sum
+       }
+       return list
+   }
 }
 
 
